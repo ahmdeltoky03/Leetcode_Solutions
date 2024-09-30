de40120c4ccadea7bf192666e75fa80b265cbbff
ahmed eltokhy <139669976+ahmdeltoky03@users.noreply.github.com>
2024-09-30 03:04:47 +0300

    [846] [Time Beats: 13.89%] [Memory Beats: 58.08%] - LeetPush

---
+++ 846-Hand-of-Straights.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+class Solution(object):
+    def isNStraightHand(self, hand, groupSize):
+        """
+        :type hand: List[int]
+        :type groupSize: int
+        :rtype: bool
+        """
+        if len(hand) % groupSize != 0:
+            return False
+        
+        count = Counter(hand)
+        while count:
+            min_card = min(count)
+            for i in range(min_card, min_card + groupSize):
+                if count[i] == 0:
+                    return False
+                count[i] -= 1
+                if count[i] == 0:
+                    del count[i]
+        
+        return True
