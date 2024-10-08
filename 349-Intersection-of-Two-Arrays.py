78c6abbc2457c11d3e21bdd4ab709cefff000f02
ahmed eltokhy <139669976+ahmdeltoky03@users.noreply.github.com>
2024-10-08 07:08:49 +0300

    [349] [Time Beats: 34.09%] [Memory Beats: 24.09%] - LeetPush

---
+++ 349-Intersection-of-Two-Arrays.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        _list = []
+        char_num1 ={}
+        n = len(nums1)
+        for i in range(n):
+            if nums1[i] in char_num1:
+                char_num1[nums1[i]] += 1
+            else:
+                char_num1[nums1[i]] = 1
+
+        m = len(nums2)
+        for i in range(m):
+            if nums2[i] in char_num1:
+                if nums2[i] not in _list:
+                    _list.append(nums2[i])
+        return _list
+        
