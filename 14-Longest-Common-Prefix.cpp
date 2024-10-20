b38ebcb00c72da4e46bc01372e6a1c6bbccffd3a
Ahmed  Eltokhey <139669976+ahmdeltoky03@users.noreply.github.com>
2024-10-20 17:56:50 +0300

    [14] [Time Beats: 27.04%] [Memory Beats: 97.06%] - LeetPush

---
+++ 14-Longest-Common-Prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+    string ans=\\;
+     for(int i=0;i<strs[0].size();i++)
+    {
+        char x = strs[0][i];//a h m e d
+        for(int g=0;g<strs.size();g++)
+        {
+            if(strs[g][i] != x)
+            {
+                return ans;
+            }
+        }
+        ans+=x;
+    }
+    return ans;
+        
+    }
+};
